13d64d5a7fa2f7a1e2d98cca2df52e19ba1171d3
Vera Compagner <s1099891@student.hsleiden.nl>
2021-07-04 11:02:02 +0200

    Timer toegevoegd is nog niet af!

--- app/src/main/java/com/example/pilreminderapp/KalenderFragment.java
+++ app/src/main/java/com/example/pilreminderapp/KalenderFragment.java
@@ -1,19 +1,73 @@
 package com.example.pilreminderapp;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class KalenderFragment extends Fragment {
+
+
+public class KalenderFragment extends Fragment{
+
+
+    public KalenderFragment(MainActivity a){
+        activity = a;
+    }
+
+    MainActivity activity;
+    Button fab;
+    EditText nameOfTimer;
+    EditText timeOfTimer;
+    TimePickerDialog timePickerDialog;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_kalender, container, false);
+        View view = inflater.inflate(R.layout.fragment_kalender, container, false);
+
+        Button fab = (Button) view.findViewById(R.id.fab);
+        nameOfTimer = view.findViewById(R.id.nameOfTimer);
+
+        timeOfTimer = view.findViewById(R.id.timeOfTimer);
+        timeOfTimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timePickerDialog = new TimePickerDialog(activity, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minuts) {
+                        timeOfTimer.setText(hourOfDay + ":" + minuts);
+                    }
+                }, 0,0, false);
+                timePickerDialog.show();
+            }
+        });
+        //delay met button's bijvoorbeeld
+
+
+
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Log.d("notescreen", "This is the text: " + nameOfTimer.getText());
+                activity.scheduleNotification(activity, 1000, 24);
+
+            }
+        });
+
+        return view;
     }
+
 }
--- app/src/main/java/com/example/pilreminderapp/MainActivity.java
+++ app/src/main/java/com/example/pilreminderapp/MainActivity.java
@@ -1,10 +1,25 @@
 package com.example.pilreminderapp;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,6 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PiloverzichtFragment(this)).commit();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.titleTime);
+            String description = getString(R.string.contentTime);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("hoi", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
     }
 
     private PiloverzichtFragment setPiloverzichtFragment(){
@@ -35,6 +62,43 @@ public class MainActivity extends AppCompatActivity {
         return faqFragment;
     }
 
+    private KalenderFragment setKalenderFragment(){
+        KalenderFragment kalenderFragment = new KalenderFragment(this);
+        return kalenderFragment;
+    }
+
+
+    public void scheduleNotification(Context context, long delay, int notificationId) {//delay is after how much time(in millis) from current time you want to schedule the notification
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.voorbeeld);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setContentTitle(context.getString(R.string.titleTime))
+                .setContentText(context.getString(R.string.contentTime))
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_baseline_calendar_today_24)
+                .setLargeIcon(largeIcon)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION));
+
+        Intent intent = new Intent(context, KalenderFragment.class);
+        PendingIntent activity = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(activity);
+
+        builder.setChannelId("hoi");
+        Notification notification = builder.build();
+
+        Intent notificationIntent = new Intent(context, MyNotificationPublisher.class);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION_ID, notificationId);
+        notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        //set de wekker Hier kan je herhalingen aan toevoegen!
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
 
 
 
@@ -47,13 +111,13 @@ public class MainActivity extends AppCompatActivity {
 
                     switch (item.getItemId()){
                         case R.id.nav_kalender:
-                            selectedFragment = new KalenderFragment();
+                            selectedFragment = setKalenderFragment();
                             break;
                         case R.id.nav_pil_overzicht:
                             selectedFragment = setPiloverzichtFragment();
                             break;
                         case R.id.nav_FAQ:
-                            selectedFragment =  setFaqFragment();
+                            selectedFragment = setFaqFragment();
                             break;
                     }
 
